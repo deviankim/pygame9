5794f25a29b054d9d74b6bfa7a64716926b3a45e
ssk <developer.kss@gmail.com>
2024-01-18 05:10:53 +0900

    07 initial

---
+++ 01-cave/07.py
@@ -0,0 +1,46 @@
+import sys
+import pygame
+from pygame.locals import QUIT, KEYDOWN, K_SPACE, Rect
+
+pygame.init()
+pygame.key.set_repeat(5, 5)
+SURFACE = pygame.display.set_mode((800, 600))
+FPSCLOCK = pygame.time.Clock()
+
+
+def main():
+    walls = 80
+    ship_y = 250
+    velocity = 0
+    ship_image = pygame.image.load("ship.png")
+
+    holes = []
+    for xpos in range(walls):
+        holes.append(Rect(xpos * 10, 100, 10, 400))
+
+    while True:
+        is_space_down = False
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN and event.key == K_SPACE:
+                is_space_down = True
+
+        velocity += -3 if is_space_down else 3
+        ship_y += velocity
+
+        SURFACE.fill((0, 255, 0))
+
+        for hole in holes:
+            pygame.draw.rect(SURFACE, (0, 0, 0), hole)
+
+        SURFACE.blit(ship_image, (0, ship_y))
+
+        pygame.display.update()
+
+        FPSCLOCK.tick(15)
+
+
+main()

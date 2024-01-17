24e7c882a22931be3eea07e041411154bffe9142
ssk <developer.kss@gmail.com>
2024-01-18 05:41:57 +0900

    07 경사 적용

--- 01-cave/07.py
+++ 01-cave/07.py
@@ -12,6 +12,7 @@ def main():
     walls = 80
     ship_y = 250
     velocity = 0
+    slope = 6  # 경사값 6으로 초기화
     ship_image = pygame.image.load("ship.png")
 
     holes = []
@@ -30,6 +31,17 @@ def main():
 
         velocity += -3 if is_space_down else 3
         ship_y += velocity
+
+        edge = holes[-1].copy()  # 배열의 가장 끝(-1) 에 있는 구멍의 복사본 생성
+        test = edge.move(0, slope)  # 구멍이 벽에 닿았는지 확인하기 위한 test 변수
+        if test.top <= 0 or test.bottom >= 600:  # 벽에 닿았는지 조건 검사
+            slope *= -1  # 경사에 -1 을 곱해 주어 반대 방향으로 전환
+
+        edge.move_ip(10, slope)  # 가장자리 구멍을 경사값 만큼 y 좌표를 이동
+        holes.append(edge)  # 가장자리를 구멍 리스트에 추가
+
+        del holes[0]  # 맨 처음 구멍을 제거
+        holes = [x.move(-10, 0) for x in holes]  # 구멍을 좌측으로 -10씩 이동 (스크롤 효과)
 
         SURFACE.fill((0, 255, 0))
 

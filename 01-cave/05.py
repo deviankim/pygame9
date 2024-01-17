7eff67a94170ef9bbecceb1b46d049ec60117782
ssk <developer.kss@gmail.com>
2024-01-17 22:31:50 +0900

    05 스페이스바를 눌러서 우주선이 위로 올라가도록 힘을 준다.

--- 01-cave/05.py
+++ 01-cave/05.py
@@ -1,26 +1,30 @@
 import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, KEYDOWN, K_SPACE  # KEYDOWN, K_SPACE 상수 가져오기
 
 pygame.init()
+pygame.key.set_repeat(5, 5)  # 키입력 반복을 지정한다. 딜레이와 간격값 입력
 SURFACE = pygame.display.set_mode((800, 600))
 
 FPSCLOCK = pygame.time.Clock()
 
 
 def main():
-    ship_y = 0
+    ship_y = 250  # 우주선의 y 좌표를 250 에서 시작 하도록 초기값 할당
     velocity = 0
     ship_image = pygame.image.load("ship.png")
 
     while True:
+        is_space_down = False  # space 가 눌러졌는지 확인하는 상태 변수
 
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == KEYDOWN and event.key == K_SPACE:  # 스페이스가 눌러졌는지 조건 검사
+                is_space_down = True
 
-        velocity += 3
+        velocity += -3 if is_space_down else 3  # 스페이스가 눌러지면 위로 힘을 주고, 아니면 아래로 힘이 가게 한다.
         ship_y += velocity
 
         SURFACE.fill((0, 255, 0))

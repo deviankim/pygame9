3b07c9b5bc1a9d4f3225b4e4601b4d84ae38b94b
ssk <developer.kss@gmail.com>
2024-01-19 05:08:58 +0900

    06 while 문 소개

--- 00-guess-a-number/06.py
+++ 00-guess-a-number/06.py
@@ -10,8 +10,10 @@ print("computer_number 는 무엇 일까요?")
 player_number = input()
 print(f"플레이어의 숫자는 {player_number} 입니다.")
 
-if computer_number == player_number:
-    print("축하합니다.")
-    print("당신이 이겼습니다.")
-else:
-    print("틀렸습니다.")
+while True:  # while 뒤의 조건이 만족할 동안 아래 블럭을 반복 합니다.
+    # 키보드로 아래 블럭을 선택한 다음, 탭(Tab)을 한번 눌러서 들여쓰기를 넣어 줍니다.
+    if computer_number == player_number:
+        print("축하합니다.")
+        print("당신이 이겼습니다.")
+    else:
+        print("틀렸습니다.")

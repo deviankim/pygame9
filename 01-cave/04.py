8e904950d4f9c26c813e2c5a3af92b86a6705e0d
ssk <developer.kss@gmail.com>
2024-01-18 22:14:26 +0900

    04 우주선에 중력 적용

--- 01-cave/04.py
+++ 01-cave/04.py
@@ -8,6 +8,8 @@ FPSCLOCK = pygame.time.Clock()
 
 
 def main():
+    ship_y = 0  # 우주선의 좌표 초기값
+    velocity = 0  # 우주선의 가속도 초기값
     ship_image = pygame.image.load("ship.png")
 
     while True:
@@ -17,9 +19,12 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+        velocity += 3  # 중력 가속도를 3씩 증가
+        ship_y += velocity  # 우주선 y 좌표에 중력 가속도를 더해서 아래로 떨어지게 한다.
+
         SURFACE.fill((0, 255, 0))
 
-        SURFACE.blit(ship_image, (0, 0))
+        SURFACE.blit(ship_image, (0, ship_y))  # ship_y 를 사용하여 y 좌표 지정
 
         pygame.display.update()
 

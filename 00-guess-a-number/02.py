6faf0d43a8d32f571e479aa3d9beabb763a49bfa
ssk <developer.kss@gmail.com>
2024-01-19 04:36:54 +0900

    02 randint 함수 사용

--- 00-guess-a-number/02.py
+++ 00-guess-a-number/02.py
@@ -1,2 +1,5 @@
 print("안녕하세요 플레이어 ??? 님")
 print("숫자 맞추기 게임을 시작 합니다.")
+
+computer_number = randint(1, 5)  # [1,2,3,4,5] 중의 하나를 랜덤 선택하여 computer_number 변수에 할당 합니다.
+print(f"[debug]: computer_number is {computer_number}")  # computer_number 가 무엇인지 print() 로 확인

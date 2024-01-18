3b6a0f48cc93d2656c1e83c657cac76f39e23d55
ssk <developer.kss@gmail.com>
2024-01-19 05:12:06 +0900

    07 while 문 영역 확장하여 입력을 계속 받도록 한다.

--- 00-guess-a-number/07.py
+++ 00-guess-a-number/07.py
@@ -6,11 +6,12 @@ print("숫자 맞추기 게임을 시작 합니다.")
 computer_number = randint(1, 5)
 print(f"[debug]: computer_number is {computer_number}")
 
-print("computer_number 는 무엇 일까요?")
-player_number = input()
-print(f"플레이어의 숫자는 {player_number} 입니다.")
-
 while True:
+    # while 문의 범위를 이곳으로 확장합니다.
+    print("computer_number 는 무엇 일까요?")
+    player_number = input()
+    print(f"플레이어의 숫자는 {player_number} 입니다.")
+
     if computer_number == player_number:
         print("축하합니다.")
         print("당신이 이겼습니다.")

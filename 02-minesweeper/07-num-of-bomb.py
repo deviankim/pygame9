72d931d4c1308380681a5b3bc8771ea7cc1de134
ssk <developer.kss@gmail.com>
2024-02-06 05:31:16 +0900

    07 initial

---
+++ 02-minesweeper/07-num-of-bomb.py
@@ -0,0 +1,91 @@
+import sys
+from math import floor
+from random import randint
+import pygame
+from pygame.locals import QUIT, MOUSEBUTTONDOWN
+
+WIDTH = 20
+HEIGHT = 15
+SIZE = 50
+NUM_OF_BOMBS = 20
+EMPTY = 0
+BOMB = 1
+OPENED = 2
+
+pygame.init()
+SURFACE = pygame.display.set_mode([WIDTH * SIZE, HEIGHT * SIZE])
+FPSCLOCK = pygame.time.Clock()
+
+
+def main():
+    largefont = pygame.font.SysFont(None, 72)
+    message_over = largefont.render("GAME OVER!!",
+                                    True, (0, 255, 225))
+    message_rect = message_over.get_rect()
+    message_rect.center = (WIDTH * SIZE / 2, HEIGHT * SIZE / 2)
+
+    game_over = False
+
+    field = [[EMPTY for xpos in range(WIDTH)]
+             for ypos in range(HEIGHT)]
+
+    count = 0
+    while count < NUM_OF_BOMBS:
+
+        xpos, ypos = randint(0, WIDTH - 1), randint(0, HEIGHT - 1)
+        if field[ypos][xpos] == EMPTY:
+            field[ypos][xpos] = BOMB
+            count += 1
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN and \
+                    event.button == 1:
+                xpos, ypos = floor(event.pos[0] / SIZE), \
+                    floor(event.pos[1] / SIZE)
+                if field[ypos][xpos] == BOMB:
+                    game_over = True
+
+        SURFACE.fill((0, 0, 0))
+        for ypos in range(HEIGHT):
+            for xpos in range(WIDTH):
+                tile = field[ypos][xpos]
+                rect = (xpos * SIZE, ypos * SIZE, SIZE, SIZE)
+
+                if tile == EMPTY or tile == BOMB:
+                    pygame.draw.rect(SURFACE,
+                                     (192, 192, 192), rect)
+                    if game_over and tile == BOMB:
+                        pygame.draw.ellipse(SURFACE,
+                                            (225, 225, 0), rect)
+
+        for index in range(0, WIDTH * SIZE, SIZE):
+            pygame.draw.line(SURFACE, (96, 96, 96),
+                             (index, 0), (index, HEIGHT * SIZE))
+        for index in range(0, HEIGHT * SIZE, SIZE):
+            pygame.draw.line(SURFACE, (96, 96, 96),
+                             (0, index), (WIDTH * SIZE, index))
+
+        for ypos in range(HEIGHT):
+            for xpos in range(WIDTH):
+                tile = field[ypos][xpos]
+                if tile == BOMB:
+                    rect = pygame.Rect(xpos * SIZE, ypos * SIZE, 10, 10)
+
+                    pygame.draw.ellipse(SURFACE, (222, 100, 0), rect)
+
+        if game_over:
+            SURFACE.blit(message_over, message_rect.topleft)
+
+        pygame.display.update()
+        FPSCLOCK.tick(15)
+
+
+if __name__ == '__main__':
+    main()
+
+'''
+'''
